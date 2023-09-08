12e4beec03526e424e0e50c5734815bdbb2f914d
Kenjesan2023 <143146547+Kenjesan2023@users.noreply.github.com>
2023-09-08 10:11:31 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -5,5 +5,13 @@ function displaySum() {
   let total = firstNum + secondNum;
   document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
 }
+document.getElementById('sumButton').addEventListener("click", displaySum);
 
+function displaySum() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum - secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
+}
 document.getElementById('sumButton').addEventListener("click", displaySum);
