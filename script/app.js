bd0c26db46379a97e55bc4d6801efde4da17779a
Kenjesan2023 <143146547+Kenjesan2023@users.noreply.github.com>
2023-09-08 10:36:26 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -13,5 +13,13 @@ function displayDiff() {
   let total = firstNum - secondNum;
   document.getElementById("diffanswer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
 }
+function displayProduct() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum * secondNum;
+  document.getElementById("proanswer").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${total}` ;
+}
 document.getElementById('sumButton').addEventListener("click", displaySum);
 document.getElementById('diffButton').addEventListener("click", displayDiff);
+document.getElementById('productButton').addEventListener("click", displayProduct);
